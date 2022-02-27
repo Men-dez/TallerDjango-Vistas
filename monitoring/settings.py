e81a04c8790f50bbcb9d9e8579c9455d72e4a7b6
Julián Andrés Méndez <j.mendezm@uniandes.edu.co>
2022-02-27 03:26:05 -0500

    Variables Ready

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "arquisoft_t3",
+        "USER": "useruni",
+        "PASSWORD": "uniandes201902",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
